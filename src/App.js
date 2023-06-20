1093ea07133e6a6533bffdd8e8d0ca73094317c4
gundam1945 <gundam1945@hotmail.com>
2023-06-20 17:34:31 +0800

    Add header to shopping cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import AppRoute from "./routes/AppRoute";
+import Header from "./components/header/Header";
 
 function App() {
     return (
-        <AppRoute />
+        <div className="App">
+            <Header>
+                <div>The Arcadia</div>
+            </Header>
+            <AppRoute />
+        </div>
     );
 }
 
